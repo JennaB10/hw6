ece27bc762107e9bfe4a3aa47730aa28afddeeb5
JennaB10 <jennb1046@gmail.com>
2021-02-14 21:08:10 -0600

    sunday

--- movies.js
+++ movies.js
@@ -1,6 +1,4 @@
-// Note: image data returned by the API will only give you the filename;
-// prepend with `https://image.tmdb.org/t/p/w500/` to get the 
-// complete image URL
+
 
 //don't create a collection
 
@@ -21,7 +19,7 @@ let db = firebase.firestore()
   let response = await fetch (`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US`) // .json?
   let json = await response.json()
   // console.dir(json)
-  let movies = json.results // individual moves are under results 
+  let movies = json.results // individual movies are under results 
   console.log(movies)
 
 
@@ -39,11 +37,31 @@ let db = firebase.firestore()
   //   <img src="https://image.tmdb.org/t/p/w500/moviePosterPath.jpg" class="w-full">
   //   <a href="#" class="watched-button block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">I've watched this!</a>
   // </div>
+
+  // Note: image data returned by the API will only give you the filename;
+// prepend with `https://image.tmdb.org/t/p/w500/` to get the 
+// complete image URL
+
   // ⬇️ ⬇️ ⬇️
 
+// function renderMovies(movies) {
+
+
+
   for (let i=0; i<movies.length; i++) {
-    let item = items[i].data()
-    item.name
+    let movie = movies[i].id
+   // let movie = movies[i].data()
+
+    document.querySelector('.movies').insertAdjacentHTML('beforeend',` 
+     <div class="w-1/5 p-4 ${movie.id}">
+      <img src="https://image.tmdb.org/t/p/w500/${movie.poster.path}" class="w-full">
+      <a href="#" class="watched-button block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">I've watched this!</a>
+     </div>
+        `)
+    
+
+ //}
+
 
   }
 
